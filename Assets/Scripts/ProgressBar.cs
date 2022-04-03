d4705252e0f3d106f1edf5b1fa5b526a88c6655c
Elbert574 <102833808+Elbert574@users.noreply.github.com>
2022-04-02 20:17:28 -0500

    Stuff

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int maximumFillBar;
+    public int currentFillBar;
+    public Image mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)currentFillBar / (float)maximumFillBar;
+        mask.fillAmount = fillAmount;
+    }
+}
